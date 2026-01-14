e84cfabb8b314337b07fe054647f9fe88bdfcf74
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-01-14 14:09:02 +0200

    Added abstract class Shape with abstract methods

---
+++ Program/Shape.cs
@@ -0,0 +1,9 @@
+﻿namespace Program
+{
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+        public abstract double CalculatePerimeter();
+        public abstract void GetInfo();
+    }
+}
