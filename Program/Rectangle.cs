cdcdcc441d6330bfaffdb297a482bb915f82339d
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-01-14 14:30:10 +0200

    Added inherited class Rectangle of class Shape

---
+++ Program/Rectangle.cs
@@ -0,0 +1,15 @@
+﻿namespace Program
+{
+    public class Rectangle
+    {
+        protected double Length;
+        protected double Width;
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+    }
+}
