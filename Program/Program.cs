458d8a056f769cdf32a3a0f5af61c8b01a8a06e4
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-01-14 14:59:48 +0200

    Created shapes array of 5 different inherited object of Shape class. Output all info about each shape. Found shape with max area

--- Program/Program.cs
+++ Program/Program.cs
@@ -4,7 +4,27 @@
     {
         public static void Main(string[] args)
         {
-
+            double maxArea = double.MinValue;
+            string maxAreaShapeName = "";
+            Shape[] shapes =
+            {
+                new Circle(5),
+                new Rectangle(5, 2.5),
+                new Triangle(17, 20, 20),
+                new Circle(6.5),
+                new Rectangle(20, 9)
+            };
+            Console.WriteLine("=== Information about shapes ===\n");
+            foreach (Shape shape in shapes)
+            {
+                shape.GetInfo();
+                if (shape.CalculateArea() > maxArea)
+                {
+                    maxArea = shape.CalculateArea();
+                    maxAreaShapeName = shape.GetType().Name;
+                }
+            }
+            Console.WriteLine($"Max Area of all the shapes: {maxAreaShapeName} {maxArea} cm2");
         }
     }
 }
