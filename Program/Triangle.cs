f772678903812b09b5d7a757ce93631276fdb0f3
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-01-14 14:37:27 +0200

    Created new inherited class of Shape class

---
+++ Program/Triangle.cs
@@ -0,0 +1,16 @@
+﻿namespace Program
+{
+    public class Triangle : Shape
+    {
+        protected double FirstSideLength;
+        protected double SecondSideLength;
+        protected double ThirdSideLength;
+
+        public Triangle(double firstSideLength, double secondSideLength, double thirdSideLength)
+        {
+            FirstSideLength = firstSideLength;
+            SecondSideLength = secondSideLength;
+            ThirdSideLength = thirdSideLength;
+        }
+    }
+}
