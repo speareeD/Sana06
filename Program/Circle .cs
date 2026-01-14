2c8033dc08df91ae9f91eced8a2320283fbcb788
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-01-14 14:17:15 +0200

    Added new class which is inherited by Shape class with own realized methods from base class

---
+++ Program/Circle .cs
@@ -0,0 +1,25 @@
+﻿namespace Program
+{
+    public class Circle : Shape
+    {
+        protected double Radius;
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Shape: Circle\nArea: {CalculateArea()}\nPerimeter: {CalculatePerimeter()}");
+        }
+    }
+}
